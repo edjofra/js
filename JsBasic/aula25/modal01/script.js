1e12a2d2bb5c4262e7465b83251b9ee79e403a13
edjofra <113058980+edjofra@users.noreply.github.com>
2024-04-11 11:27:41 -0300

    modal 01

---
+++ JsBasic/aula25/modal01/script.js
@@ -0,0 +1,9 @@
+const butao = document.querySelector('button')
+const modal = document.querySelector('dialog')
+const butaoClose = document.querySelector('dialog button')
+butao.onclick = function(){
+    modal.showModal()
+}
+butaoClose.onclick = function(){
+    modal.close()
+}
