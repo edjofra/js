b30a85495cd4212c32ab7ef14346b4e50eb214b4
edjofra <113058980+edjofra@users.noreply.github.com>
2024-05-06 07:46:31 -0300

    tela cadastro js 01

--- JsBasic/aula27/js/script.js
+++ JsBasic/aula27/js/script.js
@@ -13,21 +13,50 @@ class Validator {
         // pra ser possivel dá o looping, tenho q transofrma esse HTMLcollections em Array
        
         let inputsArray = [...inputs]
-        //loop nos iputs e validação no que for encontrado
-        inputsArray.forEach((input) => {
-            //loop com todas as validações q existem
-            for(let i = 0; this.validations.length > i; i++){
-                if(input.getAttribute(this.validations[i]) =! null){
-                    input.getAttribute(this.validations[i])
-                    console.log('ACHOOOU')
-                }
-            }
         
-
-        })
-
+        //loop nos iputs e validação no que for encontrado
+        inputsArray.forEach((input) =>{
+        // loop nas validaçoes q existem
+        for(let i = 0; this.validations.length > i; i++){
+            // verifica se a validação atual existe no input
+            if(input.getAttribute(this.validations[i]) != null){
+              //data-min-length ---> minlegth, limpando a string e transformando num medoto
+                let metodo = this.validations[i].replace('data-', '').replace('-','')
+            // valor do input 
+                let value = input.getAttribute(this.validations[i])
+            
+            // invoque o metodo
+                this[metodo](input, value)
+            }
+        }
+        },this)
 
     }
+    // met p verifica se o imput tem o numer de caractres
+    minlength(input, minValue){
+        let inputLength = input.value.length
+        let errorMessage = `O campo precisa de pelo menos ${minValue} caracteres`
+
+        if(inputLength < minValue){
+            this.printmessage(input, errorMessage)
+        }
+    }
+    // met p imprimir msgs na tela
+    printmessage(input, msg){
+        let template = document.querySelector('.error-validation').cloneNode(true);
+
+      template.textContent = msg;
+  
+      let inputParent = input.parentNode;
+  
+      template.classList.remove('template');
+  
+      inputParent.appendChild(template);
+
+    }
+
+
+
 
 }
 
