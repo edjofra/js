61eaef571df732fe4face4730797cd0b965dc814
edjofra <113058980+edjofra@users.noreply.github.com>
2024-05-08 15:28:49 -0300

    tela-validacao-js

--- JsBasic/aula27/js/script.js
+++ JsBasic/aula27/js/script.js
@@ -2,12 +2,23 @@
 class Validator {
     constructor(){
         this.validations = [
+            'data-required',
             'data-min-length',
+            'data-max-length',
+            'data-email-validate',
+            'data-only-letters',
+            'data-equal',
+            'data-pass-validation',
         ]
     }
 
 // metodo que faz a validação de todos capos
     validate(form){
+        //resgata todas validações recentes, depois limpa
+        let currentValidations = document.querySelectorAll('form .error-validation')
+        if(currentValidations.length > 0){
+            this.cleanValidations(currentValidations)
+        }
         //pegar os imputs
         let inputs = form.getElementsByTagName('input')
 
@@ -34,7 +45,8 @@ class Validator {
        
 
     }
-    // met p verifica se o imput tem o numer de caractres
+
+    // met p verifica se o imput tem o numer minimo de caractres
     minlength(input, minValue){
         let inputLength = input.value.length
         let errorMessage = `O campo precisa de pelo menos ${minValue} caracteres`
@@ -43,27 +55,103 @@ class Validator {
             this.printmessage(input, errorMessage)
         }
     }
+
+    // met p verifica se o imput tem o numero maxímo de caractres
+    maxlength(input, maxValue){
+        let inputLength = input.value.length
+        let errorMessage = `O campo precisa ter no máximo ${maxValue} caracteres`
+
+        if(inputLength > maxValue){
+            this.printmessage(input, errorMessage)
+        }
+    }
+
     // met p imprimir msgs na tela
     printmessage(input, msg){
-        let template = document.querySelector('.error-validation').cloneNode(true);
 
-      template.textContent = msg;
+        let erros = input.parentNode.querySelector('.error-validation')
+
+        if(erros === null){
+            let template = document.querySelector('.error-validation').cloneNode(true);
+
+       template.textContent = msg
   
       let inputParent = input.parentNode;
   
       template.classList.remove('template');
   
       inputParent.appendChild(template);
+        }
 
     }
 
+    // verificar se o input é requrido, se está vazio
+    required(input){
+        let inputValue = input.value
+        
+        if(inputValue === ''){
+            let erroMsg = `Este campo é obrigatório`
+
+            this.printmessage(input, erroMsg)
+        }
+    }
+
+    //limpar as validações da tela
+    cleanValidations(validations){
+
+        validations.forEach(e => e.remove())
+
+    }
+
+    //validando e-mail via RegEx
+    emailvalidate(input){
+        //email@email.com
+        let re = /\S+@\S+\.\S+/
+        let email = input.value
+        let errorMsg = 'Insira um e-mail padrão: user@email.com'
+
+        if(!re.test(email)){
+
+            this.printmessage(input, errorMsg)
+        }
+
+    }
+
+    onlyletters(input){
+        let re =  /^[A-Za-z]+$/
+        let inputValue = input.value
+        let erroMsg = 'Este campo não aceita numeros nem caracteres especiais'
+        if(!re.test(inputValue)){
+
+            this.printmessage(input, erroMsg)
+        }
+    }
+
+    equal(input, inputName){
+
+        let inputToCompare = document.getElementsByName(inputName)[0]
+
+        let erroMsg = `Este campo deve estar igual ao ${inputName}`
+
+        if(input.value != inputToCompare.value){
+            this.printmessage(input, erroMsg)
+        }
+
+    }
+
+    passvalidation(input){
+        
+    }
+
 }
+
+
+
 
 let validator = new Validator()
 // seleção de elementos
 const form = document.querySelector('#register-form')
 const submit = document.querySelector('#btn-submit')
-
 
 //evento
 submit.addEventListener('click', (e) => {
