30a64db7686c2fc0963ea3f275a7b78f250be98b
edjofra <113058980+edjofra@users.noreply.github.com>
2024-05-10 10:20:13 -0300

    tela-validation-complete-js

--- JsBasic/aula27/js/script.js
+++ JsBasic/aula27/js/script.js
@@ -66,6 +66,81 @@ class Validator {
         }
     }
 
+    // verificar se o input é requrido, se está vazio
+    required(input){
+        let inputValue = input.value
+        
+        if(inputValue === ''){
+            let erroMsg = `Este campo é obrigatório`
+
+            this.printmessage(input, erroMsg)
+        }
+    }
+
+    //validando e-mail via RegEx
+    emailvalidate(input){
+        //email@email.com
+        let re = /\S+@\S+\.\S+/
+        let email = input.value
+        let errorMsg = 'Insira um e-mail padrão: user@email.com'
+
+        if(!re.test(email)){
+
+            this.printmessage(input, errorMsg)
+        }
+
+    }
+    // verificando se tem apenas letras
+    onlyletters(input){
+        let re =  /^[A-Za-z]+$/
+        let inputValue = input.value
+        let erroMsg = 'Este campo não aceita numeros nem caracteres especiais'
+        if(!re.test(inputValue)){
+
+            this.printmessage(input, erroMsg)
+        }
+    }
+    //verificarando se as senhas são iguais
+    equal(input, inputName){
+
+        let inputToCompare = document.getElementsByName(inputName)[0]
+
+        let erroMsg = `Este campo deve estar igual ao ${inputName}`
+
+        if(input.value != inputToCompare.value){
+            this.printmessage(input, erroMsg)
+        }
+
+    }
+    // validar o campo da senha, para incluir numero e letra maiúscula
+    passvalidation(input){
+    // transformar a string em array
+    let charArr = input.value.split('')
+
+    // dois contadores 
+    let upperCase = 0
+    let numbers = 0
+
+    // vou percorrer os arrays em busca de upperCase  e numbers
+
+    for(let i = 0; charArr.length> i; i++){
+        if(charArr[i] === charArr[i].toUpperCase() && isNaN(parseInt(charArr[i]))){
+            upperCase++
+        } else if(parseInt(charArr[i])){
+            numbers++
+        }
+    }
+
+    if(upperCase === 0 || numbers === 0){
+        let erroMsg = 'A senha precisa de um caractere maiúsculo e de um número'
+
+        this.printmessage(input, erroMsg)
+    }
+
+        
+    }
+
+
     // met p imprimir msgs na tela
     printmessage(input, msg){
 
@@ -84,68 +159,14 @@ class Validator {
         }
 
     }
-
-    // verificar se o input é requrido, se está vazio
-    required(input){
-        let inputValue = input.value
-        
-        if(inputValue === ''){
-            let erroMsg = `Este campo é obrigatório`
-
-            this.printmessage(input, erroMsg)
-        }
-    }
-
     //limpar as validações da tela
     cleanValidations(validations){
 
         validations.forEach(e => e.remove())
-
-    }
-
-    //validando e-mail via RegEx
-    emailvalidate(input){
-        //email@email.com
-        let re = /\S+@\S+\.\S+/
-        let email = input.value
-        let errorMsg = 'Insira um e-mail padrão: user@email.com'
-
-        if(!re.test(email)){
-
-            this.printmessage(input, errorMsg)
-        }
-
-    }
-
-    onlyletters(input){
-        let re =  /^[A-Za-z]+$/
-        let inputValue = input.value
-        let erroMsg = 'Este campo não aceita numeros nem caracteres especiais'
-        if(!re.test(inputValue)){
-
-            this.printmessage(input, erroMsg)
-        }
-    }
-
-    equal(input, inputName){
-
-        let inputToCompare = document.getElementsByName(inputName)[0]
-
-        let erroMsg = `Este campo deve estar igual ao ${inputName}`
-
-        if(input.value != inputToCompare.value){
-            this.printmessage(input, erroMsg)
-        }
-
-    }
-
-    passvalidation(input){
-        
+    
     }
 
 }
-
-
 
 
 let validator = new Validator()
